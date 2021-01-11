860f1767aebb3f7d4633ba3a977d8f266233693e
othmane mdarhri <outhman.mdarhri@gmail.com>
2021-01-11 02:48:07 +0100

    finishing html for navbar (#2)

--- index.php
+++ index.php
@@ -1,15 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AMOIL</title>
     <link href="css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-<script src="js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    <link href="css/navbar.css" rel="stylesheet" >
+    <script src="js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/bootstrap-icons.css">
 </head>
+
 <body>
-    <div class="jumbotron">
-    <h1>hello</h1>
-    </div>
+    <?php
+    require_once "req/navbar.php";
+
+    ?>
 </body>
+
 </html>
