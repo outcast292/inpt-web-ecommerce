fb1ab793ebe112ea3e6a2e9340375bfcc232503e
simo0077 <lembamed100@gmail.com>
2021-01-11 23:22:49 +0100

    creation navigation.php

--- navigation.php
+++ navigation.php
@@ -1,21 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>AMOIL</title>
+    <title>Document</title>
     <link href="css/bootstrap.min.css" rel="stylesheet"  >
     <link href="css/navbar.css" rel="stylesheet" >
     <script src="js/bootstrap.bundle.min.js"  ></script>
     <link rel="stylesheet" href="css/bootstrap-icons.css">
 </head>
-
 <body>
-    <?php
+<?php
     require_once "req/navbar.php";
 
     ?>
 </body>
-
 </html>
