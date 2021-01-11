0afe50bf8d845f305614d36f0e5a7fc776025e00
Othmane Mdarhri <outhman.mdarhri@gmail.com>
2021-01-11 02:45:27 +0100

    finishing html for navbar

--- index.php
+++ index.php
@@ -1,15 +1,216 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AMOIL</title>
     <link href="css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-<script src="js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    <link href="css/navbar.css" rel="stylesheet">
+    <script src="js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/bootstrap-icons.css">
+    <style>
+        .carousel .carousel-item {
+            height: 300px;
+        }
+
+        .carousel-item img {
+            position: absolute;
+            top: 0;
+            left: 0;
+            min-height: 300px;
+        }
+    </style>
 </head>
+
 <body>
-    <div class="jumbotron">
-    <h1>hello</h1>
+    <?php
+    require_once "req/navbar.php";
+    ?>
+    <div class="container bg-light p-2">
+        <div class="m-1">
+            <form action="" method="get">
+                <label for="search_input" class="form-label">Vous recherchez quelque chose ?</label>
+                <input class="form-control" type="search" name="search_input" id="search_input">
+            </form>
+        </div>
+
+
+        <div class="row">
+
+            <div class="col-3 bg-secondary p-1">
+                <ul class="nav nav-tabs flex-column">
+                    <li class="nav-item">
+                        <div class="d-grid gap-2">
+                            <div class="btn-group dropend">
+                                <button type="button" class="btn   dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Dropright
+                                </button>
+                                <ul class="dropdown-menu">
+                                    <li>
+                                        <h6 class="dropdown-header">Dropdown header</h6>
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">Action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="nav-item ">
+                        <div class="d-grid gap-2">
+                            <div class="btn-group dropend">
+                                <button type="button" class="btn   dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Dropright
+                                </button>
+                                <ul class="dropdown-menu">
+                                    <li>
+                                        <h6 class="dropdown-header">Dropdown header</h6>
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">Action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <div class="d-grid gap-2">
+                            <div class="btn-group dropend">
+                                <button type="button" class="btn   dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Dropright
+                                </button>
+                                <ul class="dropdown-menu">
+                                    <li>
+                                        <h6 class="dropdown-header">Dropdown header</h6>
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">Action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <div class="d-grid gap-2">
+                            <div class="btn-group dropend">
+                                <button type="button" class="btn   dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Dropright
+                                </button>
+                                <ul class="dropdown-menu">
+                                    <li>
+                                        <h6 class="dropdown-header">Dropdown header</h6>
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">Action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <div class="d-grid gap-2">
+                            <div class="btn-group dropend">
+                                <button type="button" class="btn   dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Dropright
+                                </button>
+                                <ul class="dropdown-menu">
+                                    <li>
+                                        <h6 class="dropdown-header">Dropdown header</h6>
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">Action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <div class="d-grid gap-2">
+                            <div class="btn-group dropend">
+                                <button type="button" class="btn   dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Dropright
+                                </button>
+                                <ul class="dropdown-menu">
+                                    <li>
+                                        <h6 class="dropdown-header">Dropdown header</h6>
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">Action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <div class="d-grid gap-2">
+                            <div class="btn-group dropend">
+                                <button type="button" class="btn   dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Dropright
+                                </button>
+                                <ul class="dropdown-menu">
+                                    <li>
+                                        <h6 class="dropdown-header">Dropdown header</h6>
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">Action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <div class="d-grid gap-2">
+                            <div class="btn-group dropend">
+                                <button type="button" class="btn   dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Dropright
+                                </button>
+                                <ul class="dropdown-menu">
+                                    <li>
+                                        <h6 class="dropdown-header">Dropdown header</h6>
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">Action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-9">
+                <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"></li>
+                        <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"></li>
+                        <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg" class="d-block w-100 " alt="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/250px-Image_created_with_a_mobile_phone.png" class="d-block w-100" alt="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg" class="d-block w-100" alt="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg">
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
+
+
+
 </body>
+
 </html>
