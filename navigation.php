8874df1e10327f7a351a979abc3d77216c8baec0
simo0077 <lembamed100@gmail.com>
2021-01-12 05:27:35 +0100

    ajout du footer cree par othmane

--- navigation.php
+++ navigation.php
@@ -381,6 +381,27 @@
     </div>
 
   </div>
+  <div class="row bg-dark text-white ">
+    <div class="col-4 p-2">
+        <h3 class="offset-1">payment</h3>
+        <hr>
+        <p class="offset-1">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore dolor ex ea odit doloribus omnis aut dolorem quo odio incidunt porro ipsam voluptas quae alias possimus tempora, error eos expedita?</p>
+    </div>
+    <div class="col-4 p-2">
+        <h3>reseau socs</h3>
+        <hr>
+        <p><a href=""><i class="bi bi-instagram"></i>Instagram</a></p>
+        <p><a href=""><i class="bi bi-facebook"></i>Instagram</a></p>
+        <p><a href=""><i class="bi bi-linkedin"></i>Instagram</a></p>
+    </div>
+    <div class="col-4 p-2">
+        <h3>des Infos</h3>
+        <hr>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore dolor ex ea odit doloribus omnis aut dolorem quo odio incidunt porro ipsam voluptas quae alias possimus tempora, error eos expedita?</p>
+    </div>
+</div>
 
             
 </body>
