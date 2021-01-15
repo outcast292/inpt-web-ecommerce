4f3a971f0940ba6d1f7e7dbf61e024df58ccf940
Amal Kalim <amalkalim@gmail.com>
2021-01-15 01:05:59 +0100

    en cours de modif

--- produit.php
+++ produit.php
@@ -30,34 +30,34 @@
             <div class="card mb-3">
                 <div class="row ">
                     <!--premiere section-->
-                    <div class="col-md-4">
+                    <div class="col-12 col-md-6">
                         <div class="row">
                             <div class="col">
-                                <div class="card mx-3 my-3 ">
-                                    <img src="img/products/pc.jpg" alt="" class="card-img">
-                                </div>
+
+                                <img src="img/pc2.jpeg" alt="" class="card-img" style="max-height: 400px;">
+
                             </div>
                         </div>
-                        <!--d'autres photos-->
-                        <div class="row mx-3">
+                        <!--d'autres photos (ajouter un carroussel apres)-->
+                        <div class="row mx-3 my-2">
                             <div class="col-3">
                                 <div class="card ">
-                                    <a href=""><img src="img/products/pc.jpg" alt="" class="card-img"></a>
+                                    <a href=""><img src="img/pc2.jpeg" alt="" class="card-img"></a>
                                 </div>
                             </div>
                             <div class="col-3">
                                 <div class="card ">
-                                    <a href=""><img src="img/products/pc.jpg" alt="" class="card-img"></a>
+                                    <a href=""><img src="img/pc2.jpeg" alt="" class="card-img"></a>
                                 </div>
                             </div>
                             <div class="col-3">
                                 <div class="card ">
-                                    <a href=""><img src="img/products/pc.jpg" alt="" class="card-img"></a>
+                                    <a href=""><img src="img/pc2.jpeg" alt="" class="card-img"></a>
                                 </div>
                             </div>
                             <div class="col-3">
                                 <div class="card ">
-                                    <a href=""><img src="img/products/pc.jpg" alt="" class="card-img"></a>
+                                    <a href=""><img src="img/pc2.jpeg" alt="" class="card-img"></a>
                                 </div>
                             </div>
                         </div>
@@ -71,7 +71,7 @@
                     </div>
 
                     <!--deuxieme section-->
-                    <div class="col-md-8">
+                    <div class="col-12 col-md-6">
                         <div class="card-body">
                             <div class="row">
                                 <div class="col-9">
@@ -84,8 +84,19 @@
                                 </div>
 
                             </div>
-                            <img src="./img/stars.jpg" alt="rating" class="  ">
-                            <h4 class="mx-3" style="color: red;">5599,99 MAD</h4>
+                            <div>
+                                <ul class="list-inline pull-right">
+                                    <li>
+                                        <i class="bi bi-star-fill" style="color: yellow;"></i>
+                                        <i class="bi bi-star-fill" style="color: yellow;"></i>
+                                        <i class="bi bi-star-fill" style="color: yellow;"></i>
+                                        <i class="bi bi-star-half" style="color: yellow;"></i>
+                                        <i class="bi bi-star" style="color: yellow;"></i>
+
+                                    </li>
+                                </ul>
+                            </div>
+                            <h4  style="color: red;">5599,99 MAD</h4>
                             <h5 class="card-text mt-5 mx-3 "><small class="text-muted">Options</small></h5>
                             <ul class="card-text "><small class="text-muted">
                                     <li>Processeur : Intel Pentium Silver N5000 / 1.1 GHz</li>
@@ -131,7 +142,7 @@
                             <div class="tab-pane active" id="p1">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores molestiae assumenda aliquid, voluptatibus incidunt repellat nesciunt praesentium recusandae aliquam necessitatibus, molestias ullam corporis. Aliquam voluptatem ut fugit cumque et atque!</div>
                             <div class="tab-pane" id="p2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique modi earum temporibus, optio iste sed, voluptate fugiat ducimus rerum delectus quibusdam non. Suscipit, velit. Quod voluptates molestias enim ut. Asperiores.</div>
                             <div class="tab-pane" id="p3">
-                                
+
                                 <div class="card mx-3 my-2">
                                     <div class="card-body">
                                         <h6 class="card-title ">User x</h6>
@@ -196,7 +207,21 @@
                             <div class="row">
                                 <div class="col-12 col-lg-3">
                                     <div class="card">
-                                        <img src="img/products/pc.jpg" class="card-img-top" alt="...">
+                                        <img src="img/pc2.jpeg" class="card-img-top" alt="..." style="max-height: 10px;">
+                                        <div class="card-body">
+                                            <a class="stretched-link" href="#" style="color: rgb(2, 2, 58);">
+                                                <span class="card-title h4">pc</span>
+                                            </a>
+                                            
+                                            <p style="color:gray">description</p>
+                                            <a href="#" class="btn btn-danger">Acheter</a>
+                                            <span class="h6" style="margin-left:20px;">3499,99 MAD</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-lg-3">
+                                    <div class="card">
+                                        <img src="img/pc2.jpeg" class="card-img-top" alt="...">
                                         <div class="card-body">
                                             <a class="stretched-link" href="#" style="color: rgb(2, 2, 58);">
                                                 <span class="card-title h4">pc</span>
@@ -210,21 +235,7 @@
                                 </div>
                                 <div class="col-12 col-lg-3">
                                     <div class="card">
-                                        <img src="img/products/pc.jpg" class="card-img-top" alt="...">
-                                        <div class="card-body">
-                                            <a class="stretched-link" href="#" style="color: rgb(2, 2, 58);">
-                                                <span class="card-title h4">pc</span>
-                                            </a>
-                                            <img src="./img/stars.jpg" alt="product" class="w-25">
-                                            <p style="color:gray">description</p>
-                                            <a href="#" class="btn btn-danger">Acheter</a>
-                                            <span class="h6" style="margin-left:20px;">3499,99 MAD</span>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-12 col-lg-3">
-                                    <div class="card">
-                                        <img src="img/products/pc.jpg" class="card-img-top" alt="...">
+                                        <img src="img/pc2.jpeg" class="card-img-top" alt="...">
                                         <div class="card-body">
                                             <a class="stretched-link" href="#" style="color: rgb(2, 2, 58);">
                                                 <span class="card-title h4">pc</span>
@@ -238,7 +249,7 @@
                                 </div>
                                 <div class="col-12 col-lg-3">
                                     <div class="card ">
-                                        <img src="img/products/pc.jpg" class="card-img-top" alt="...">
+                                        <img src="img/pc2.jpeg" class="card-img-top" alt="...">
                                         <div class="card-body">
                                             <a class="stretched-link" href="#" style="color: rgb(2, 2, 58);">
                                                 <span class="card-title h4">pc</span>
@@ -257,7 +268,7 @@
                             <div class="row">
                                 <div class="col-12 col-lg-3">
                                     <div class="card">
-                                        <img src="img/products/pc.jpg" class="card-img-top" alt="...">
+                                        <img src="img/pc2.jpeg" class="card-img-top" alt="...">
                                         <div class="card-body">
                                             <a class="stretched-link" href="#" style="color: rgb(2, 2, 58);">
                                                 <span class="card-title h4">pc</span>
@@ -271,7 +282,7 @@
                                 </div>
                                 <div class="col-12 col-lg-3">
                                     <div class="card">
-                                        <img src="img/products/pc.jpg" class="card-img-top" alt="...">
+                                        <img src="img/pc2.jpeg" class="card-img-top" alt="...">
                                         <div class="card-body">
                                             <a class="stretched-link" href="#" style="color: rgb(2, 2, 58);">
                                                 <span class="card-title h4">pc</span>
@@ -285,7 +296,7 @@
                                 </div>
                                 <div class="col-12 col-lg-3">
                                     <div class="card">
-                                        <img src="img/products/pc.jpg" class="card-img-top" alt="...">
+                                        <img src="img/pc2.jpeg" class="card-img-top" alt="...">
                                         <div class="card-body">
                                             <a class="stretched-link" href="#" style="color: rgb(2, 2, 58);">
                                                 <span class="card-title h4">pc</span>
@@ -299,7 +310,7 @@
                                 </div>
                                 <div class="col-12 col-lg-3">
                                     <div class="card ">
-                                        <img src="img/products/pc.jpg" class="card-img-top" alt="...">
+                                        <img src="img/pc2.jpeg" class="card-img-top" alt="...">
                                         <div class="card-body">
                                             <a class="stretched-link" href="#" style="color: rgb(2, 2, 58);">
                                                 <span class="card-title h4">pc</span>
@@ -334,7 +345,7 @@
                             <div class="row">
                                 <div class="col-12 col-lg-3">
                                     <div class="card">
-                                        <img src="img/products/pc.jpg" class="card-img-top" alt="...">
+                                        <img src="img/pc2.jpeg" class="card-img-top" alt="...">
                                         <!--div class="card-img-overlay " ><a href=""><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-suit-heart d-inline-flex align-self-r" viewBox="0 0 16 16" color="red" >
                                     <path d="M8 6.236l-.894-1.789c-.222-.443-.607-1.08-1.152-1.595C5.418 2.345 4.776 2 4 2 2.324 2 1 3.326 1 4.92c0 1.211.554 2.066 1.868 3.37.337.334.721.695 1.146 1.093C5.122 10.423 6.5 11.717 8 13.447c1.5-1.73 2.878-3.024 3.986-4.064.425-.398.81-.76 1.146-1.093C14.446 6.986 15 6.131 15 4.92 15 3.326 13.676 2 12 2c-.777 0-1.418.345-1.954.852-.545.515-.93 1.152-1.152 1.595L8 6.236zm.392 8.292a.513.513 0 0 1-.784 0c-1.601-1.902-3.05-3.262-4.243-4.381C1.3 8.208 0 6.989 0 4.92 0 2.755 1.79 1 4 1c1.6 0 2.719 1.05 3.404 2.008.26.365.458.716.596.992a7.55 7.55 0 0 1 .596-.992C9.281 2.049 10.4 1 12 1c2.21 0 4 1.755 4 3.92 0 2.069-1.3 3.288-3.365 5.227-1.193 1.12-2.642 2.48-4.243 4.38z"/>
                                   </svg></a></div-->
@@ -351,7 +362,7 @@
                                 </div>
                                 <div class="col-12 col-lg-3">
                                     <div class="card">
-                                        <img src="img/products/pc.jpg" class="card-img-top" alt="...">
+                                        <img src="img/pc2.jpeg" class="card-img-top" alt="...">
                                         <div class="card-body">
                                             <a class="stretched-link" href="#" style="color: rgb(2, 2, 58);">
                                                 <span class="card-title h4">pc</span>
@@ -365,7 +376,7 @@
                                 </div>
                                 <div class="col-12 col-lg-3">
                                     <div class="card">
-                                        <img src="img/products/pc.jpg" class="card-img-top" alt="...">
+                                        <img src="img/pc2.jpeg" class="card-img-top" alt="...">
                                         <div class="card-body">
                                             <a class="stretched-link" href="#" style="color: rgb(2, 2, 58);">
                                                 <span class="card-title h4">pc</span>
@@ -379,7 +390,7 @@
                                 </div>
                                 <div class="col-12 col-lg-3">
                                     <div class="card ">
-                                        <img src="img/products/pc.jpg" class="card-img-top" alt="...">
+                                        <img src="img/pc2.jpeg" class="card-img-top" alt="...">
                                         <div class="card-body">
                                             <a class="stretched-link" href="#" style="color: rgb(2, 2, 58);">
                                                 <span class="card-title h4">pc</span>
@@ -398,7 +409,7 @@
                             <div class="row">
                                 <div class="col-12 col-lg-3">
                                     <div class="card">
-                                        <img src="img/products/pc.jpg" class="card-img-top" alt="...">
+                                        <img src="img/pc2.jpeg" class="card-img-top" alt="...">
                                         <div class="card-body">
                                             <a class="stretched-link" href="#" style="color: rgb(2, 2, 58);">
                                                 <span class="card-title h4">pc</span>
@@ -412,7 +423,7 @@
                                 </div>
                                 <div class="col-12 col-lg-3">
                                     <div class="card">
-                                        <img src="img/products/pc.jpg" class="card-img-top" alt="...">
+                                        <img src="img/pc2.jpeg" class="card-img-top" alt="...">
                                         <div class="card-body">
                                             <a class="stretched-link" href="#" style="color: rgb(2, 2, 58);">
                                                 <span class="card-title h4">pc</span>
@@ -426,7 +437,7 @@
                                 </div>
                                 <div class="col-12 col-lg-3">
                                     <div class="card">
-                                        <img src="img/products/pc.jpg" class="card-img-top" alt="...">
+                                        <img src="img/pc2.jpeg" class="card-img-top" alt="...">
                                         <div class="card-body">
                                             <a class="stretched-link" href="#" style="color: rgb(2, 2, 58);">
                                                 <span class="card-title h4">pc</span>
@@ -440,7 +451,7 @@
                                 </div>
                                 <div class="col-12 col-lg-3">
                                     <div class="card ">
-                                        <img src="img/products/pc.jpg" class="card-img-top" alt="...">
+                                        <img src="img/pc2.jpeg" class="card-img-top" alt="...">
                                         <div class="card-body">
                                             <a class="stretched-link" href="#" style="color: rgb(2, 2, 58);">
                                                 <span class="card-title h4">pc</span>
@@ -487,7 +498,7 @@
                 })
             })
         </script>
-        <?php require_once "req/footbar.php"; ?>
+        <!--?php require_once "req/footbar.php"; ?-->
     </body>
 
 </html>
