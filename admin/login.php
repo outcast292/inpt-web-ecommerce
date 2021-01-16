d867681cd9f518e3a31751bd12a5c454421c7d38
Othmane Mdarhri <outhman.mdarhri@gmail.com>
2021-01-16 23:50:20 +0100

    login finished + basic index.php for admin

---
+++ admin/login.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Login d'admin</title>
+    <?php
+    session_start();
+    $x = 0;
+    if (isset($_POST["password"])) {
+        require "../php/connection/db.php";
+        $email_user = htmlspecialchars($_POST["email"]);
+        $pass = $_POST["password"];
+        $query = "SELECT  mdp_user,nom_user FROM utilisateurs where email_user = :email_user ";
+        $sql = $conn->prepare($query);
+        $sql->execute(array("email_user" => $email_user));
+        $row = $sql->fetch();
+        if (password_verify($pass, $row["mdp_user"])) {
+            $_SESSION["email_user"] = $email_user;
+            $_SESSION["nom_user"] = $row["nom_user"];
+            $_SESSION["connection_status"] = "connected";
+            header("location: index.php");
+        } else {
+            $x = 1;
+        }
+    }
+
+    ?>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="../css/admin/login.css" rel="stylesheet">
+</head>
+
+<body>
+    <form class="form-signin" method="post">
+        <div class="text-center mb-4">
+            <img class="mb-4" src="../../img/logo.png" alt="" width="72" height="72">
+            <h1 class="h3 mb-3 font-weight-normal">AMOIL</h1>
+            <p>Cette interface est reservée a l'administration de site AMOIl</p>
+        </div>
+
+        <div class="form-label-group">
+            <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+            <label for="inputEmail">Adresse email</label>
+        </div>
+
+        <div class="form-label-group">
+            <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
+            <label for="inputPassword">Mot de passe</label>
+        </div>
+
+        <div>
+            <?php if ($x != 0) {  ?>
+                <div class="alert alert-danger">
+                    Données invalides
+                </div>
+
+            <?php } ?>
+
+        </div>
+
+
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Se connecter</button>
+        <p class="mt-5 mb-3 text-muted text-center">&copy; 2021</p>
+    </form>
+</body>
+
+</html>
---
+++ admin/req/sidebar.php
@@ -0,0 +1,45 @@
+<div class="col-2 collapse  show d-md-flex bg-secondary  text-light   pt-2 pl-0 min-vh-100" id="sidebar">
+    <div class="row">
+
+        <h1 class="col-12  text-center">AMOIL</h1>
+        <h6 class="col-12  text-center">Interface d'administration de site AMOIl</h6>
+
+        <div class="col-12">
+            <hr>
+            <ul class="nav flex-column flex-nowrap overflow-hidden">
+                <li class="nav-item">
+                    <a class="nav-link text-truncate" href="#"><i class="bi bi-house-fill"></i> <span class="d-none d-sm-inline">principale</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link collapsed text-truncate" href="#submenu1" data-toggle="collapse" data-target="#submenu1"><i class="bi bi-bag"></i> <span class="d-none d-sm-inline">Commandes</span></a>
+                    <div class="collapse" id="submenu1" aria-expanded="false">
+                        <ul class="flex-column pl-2 nav">
+                            <li class="nav-item"><a class="nav-link py-0" href="#"><i class="bi bi-bag-check"></i><span>Validation des commandes</span></a></li>
+                            <li class="nav-item"><a class="nav-link py-0" href="#"><i class="bi bi-clock-history"></i><span>Historique</span></a></li>
+
+                        </ul>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-truncate" href="#"><i class="bi bi-cart-check"></i> <span class="d-none d-sm-inline">Produits</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-truncate" href="#"><i class="bi bi-tags-fill"></i> <span class="d-none d-sm-inline">Categories</span></a>
+                </li>
+                <li class="nav-item"><a class="nav-link text-truncate" href="#"><i class="bi bi-person-circle"></i> <span class="d-none d-sm-inline">Clients</span></a></li>
+            </ul>
+        </div>
+
+
+        <div class="col-12 text-center">
+
+        <button class="btn btn-danger"> se decconecter</button>
+
+        </div>
+
+    </div>
+
+
+
+
+</div>
