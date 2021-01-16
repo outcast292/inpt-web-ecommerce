3625101dafe2e9423c31274c03d788bc3752203f
Othmane Mdarhri <outhman.mdarhri@gmail.com>
2021-01-16 14:31:18 +0100

    fixing navbars

--- produit.php
+++ produit.php
@@ -491,9 +491,7 @@
 
             ?>
         </div>
-        <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.0/umd/popper.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
+
         <script>
             $(function() {
                 $('form').submit(function(e) {
