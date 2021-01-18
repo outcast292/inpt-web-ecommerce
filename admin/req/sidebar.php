cb878f501085f4e3e6db1df2da3768891b1aec01
Othmane Mdarhri <outhman.mdarhri@gmail.com>
2021-01-18 15:24:48 +0100

    V0.1 of commandes

--- admin/req/sidebar.php
+++ admin/req/sidebar.php
@@ -14,19 +14,19 @@
                     <a class="nav-link collapsed text-truncate" href="#submenu1" data-toggle="collapse" data-target="#submenu1"><i class="bi bi-bag"></i> <span class="d-none d-sm-inline">Commandes</span></a>
                     <div class="collapse" id="submenu1" aria-expanded="false">
                         <ul class="flex-column pl-2 nav">
-                            <li class="nav-item"><a class="nav-link py-0" href="#"><i class="bi bi-bag-check"></i><span>Validation des commandes</span></a></li>
-                            <li class="nav-item"><a class="nav-link py-0" href="#"><i class="bi bi-clock-history"></i><span>Historique</span></a></li>
+                            <li class="nav-item"><a class="nav-link py-0" href="./commandes.php"><i class="bi bi-bag-check"></i><span>Validation des commandes</span></a></li>
+                            <li class="nav-item"><a class="nav-link py-0" href="./commandes_hist.php"><i class="bi bi-clock-history"></i><span>Historique</span></a></li>
 
                         </ul>
                     </div>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-truncate" href="#"><i class="bi bi-cart-check"></i> <span class="d-none d-sm-inline">Produits</span></a>
+                    <a class="nav-link text-truncate" href="./produits.php"><i class="bi bi-cart-check"></i> <span class="d-none d-sm-inline">Produits</span></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-truncate" href="#"><i class="bi bi-tags-fill"></i> <span class="d-none d-sm-inline">Categories</span></a>
+                    <a class="nav-link text-truncate" href="./categories.php"><i class="bi bi-tags-fill"></i> <span class="d-none d-sm-inline">Categories</span></a>
                 </li>
-                <li class="nav-item"><a class="nav-link text-truncate" href="#"><i class="bi bi-person-circle"></i> <span class="d-none d-sm-inline">Clients</span></a></li>
+                <li class="nav-item"><a class="nav-link text-truncate" href="./clients.php"><i class="bi bi-person-circle"></i> <span class="d-none d-sm-inline">Clients</span></a></li>
             </ul>
         </div>
 
