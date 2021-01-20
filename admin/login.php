7487ff5d75c7f6353affbdb1af94775e8d4d2b85
Othmane Mdarhri <outhman.mdarhri@gmail.com>
2021-01-20 02:48:42 +0100

    finishing the commandes section

--- admin/login.php
+++ admin/login.php
@@ -15,13 +15,14 @@
         require "../php/connection/db.php";
         $email_user = htmlspecialchars($_POST["email"]);
         $pass = $_POST["password"];
-        $query = "SELECT  mdp_user,nom_user FROM utilisateurs where email_user = :email_user ";
+        $query = "SELECT  mdp_user,nom_user,id_user FROM utilisateurs where email_user = :email_user ";
         $sql = $conn->prepare($query);
         $sql->execute(array("email_user" => $email_user));
         $row = $sql->fetch();
         if (password_verify($pass, $row["mdp_user"])) {
             $_SESSION["email_user"] = $email_user;
             $_SESSION["nom_user"] = $row["nom_user"];
+            $_SESSION["id_user"] = $row["id_user"];
             $_SESSION["connection_status"] = "connected";
             header("location: index.php");
         } else {
--- php/commandes/commande_read.php
+++ php/commandes/commande_read.php
@@ -3,7 +3,7 @@ require_once "../connection/db.php";
 //require_once "../verify_session.php";
 #TODO add security
 if (isset($_GET["id_commande"])) {
-    $query = 'SELECT   co.date_commande,co.prix_commande,co.etat_actuell ,co.date_etat_actuel    FROM commande co    where  co.id_commande=:id_commande';
+    $query = 'SELECT   co.date_commande,co.prix_commande,co.etat_actuell ,co.date_etat_actuel, CONCAT(ad.nom_complet ,"\n",ad.adresse,"\n" , ad.code_postal) as adresse     FROM commande co left join adresse ad on co.id_client =ad.id_adresse    where  co.id_commande=:id_commande';
     $query2 = 'SELECT   co.id_produit,co.prix_produit_commande,co.qtt_commande, pr.label, m.nom_marque  FROM qt_commande co left join produit pr on co.id_produit=pr.id_produit left join  marque m on m.id_marque=pr.id_marque   where  co.id_commande=:id_commande';
     $sql = $conn->prepare($query);
     $sql->execute(array("id_commande" => $_GET["id_commande"]));
